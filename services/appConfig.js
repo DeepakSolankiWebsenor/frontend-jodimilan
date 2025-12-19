2d606d09c2cd82ab483e07163fe3bcb15de4b62d
Deepak Kumar Solanki <deepaksolanki.websenor@gmail.com>
2025-12-19 15:44:09 +0530

    url changes on 19 dec

--- services/appConfig.js
+++ services/appConfig.js
@@ -1,11 +1,11 @@
 // export const payment_url = "https://admin.jodimilan.com/ccavenue/";
-export const payment_url = "http://localhost:3006/api/";
-// export const payment_url = "https://backend-jodimilanf.onrender.com/api/";
+// export const payment_url = "http://localhost:3006/api/";
+export const payment_url = "https://backend-jodimilanf.onrender.com/api/";
 
 export const decrypted_key = "bsCL1j+niPjVN2MAm89+bTKYny2Ji0v9GMFRZPFWWYY=";
 
-// export const API_BASE_URL = "https://backend-jodimilanf.onrender.com/api/";
-export const API_BASE_URL = "http://localhost:3006/api/";
+export const API_BASE_URL = "https://backend-jodimilanf.onrender.com/api/";
+// export const API_BASE_URL = "http://localhost:3006/api/";
 
 
 export const firebaseConfig = {
